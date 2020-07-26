a15c6987e2f0a704ff3ba8f4ba84b8a62601a920
Sharan Pai <57347192+sen-pai@users.noreply.github.com>
2020-07-26 11:40:23 +0530

    added dataloader

---
+++ dataloader.py
@@ -0,0 +1,95 @@
+import numpy as np
+import os
+
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms.functional as TF
+import transformers
+
+from utils.data_util import parse_sentihood_json, convert_input, aspect2idx, all_aspects_logits
+
+"""
+Inputs:
+Raw JSON file
+
+Outputs:
+
+"""
+
+def pretrained_embeddings(text, pretrained_model):
+    #takes text as input
+    #returns bert embeddings and index of target
+    tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+    tokenized = tokenizer.encode(text, add_special_tokens= False)
+    input_ids = torch.tensor(tokenized).unsqueeze(0)  # Batch size 1
+    outputs = pretrained_model(input_ids)
+    last_hidden_states = outputs[0]
+    #encoded LOCATION = 3295
+    target_index = [i for i, j in enumerate(tokenized) if j == 3295]
+
+    return last_hidden_states, target_index
+
+def aspect_embeddings(aspect, pretrained_model):
+    #takes text as input
+    #returns bert embeddings and index of target
+    tokenizer = transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+    tokenized = tokenizer.encode(aspect, add_special_tokens= False)
+    input_ids = torch.tensor(tokenized).unsqueeze(0)  # Batch size 1
+    outputs = pretrained_model(input_ids)
+    last_hidden_states = outputs[0]
+
+    return last_hidden_states
+
+def sentiment2onehot(sentiment):
+    one_hot = np.zeros(3)
+    if sentiment == "Positive":
+        one_hot[0] = 1
+    if sentiment == "None":
+        one_hot[1] = 1
+    else:
+        one_hot[2] = 1
+    return one_hot
+
+class SentihoodDataset(Dataset):
+    def __init__(
+        self,
+        data_dir_path,
+        dataset_type='train',
+        transform=None,
+        condition_on_number = True,
+    ):
+        # try to change back here everytime
+        # self.current_path = os.getcwd()
+        self.dataset_type = dataset_type
+        self.transform = transform
+        self.condition_on_number = condition_on_number
+
+        self.embedding_pretrained_model = transformers.BertModel.from_pretrained('bert-base-uncased')
+
+        self.data_dir_path = data_dir_path  # data folder
+        os.chdir(self.data_dir_path)
+        if self.dataset_type == "train":
+            self.data = parse_sentihood_json("sentihood-train.json")
+        elif self.dataset_type == "dev":
+            self.data = parse_sentihood_json("sentihood-dev.json")
+        elif self.dataset_type == "test":
+            self.data = parse_sentihood_json("sentihood-test.json")
+
+        self.data = convert_input(self.data, aspect2idx)
+        self.aspect_logits = all_aspects_logits(self.data, aspect2idx)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, i):
+        id, text, target, aspect, sentiment = self.data[i]
+        embedded_text, target_index = pretrained_embeddings(text, self.embedding_pretrained_model)
+        sentiment_one_hot = sentiment2onehot(sentiment)
+        aspect_logit = self.aspect_logits[i//12]
+        #condition of aspect number or aspect embeddings
+        if self.condition_on_number:
+            c_aspect = aspect2idx[aspect]
+        else:
+            c_aspect = aspect_embeddings(aspect,self.embedding_pretrained_model )
+
+        return embedded_text, target_index, aspect_logit, c_aspect, sentiment_one_hot
---
+++ utils/data_util.py
@@ -0,0 +1,81 @@
+# taken from https://github.com/firascherif/ABSA-BERT-pair/blob/master/generate/data_utils_sentihood.py
+# Reference: https://github.com/liufly/delayed-memory-update-entnet
+
+from __future__ import absolute_import
+import json
+import operator
+import os
+import re
+import sys
+import xml.etree.ElementTree
+
+# import nltk
+import numpy as np
+
+
+def parse_sentihood_json(in_file):
+    with open(in_file) as f:
+        data = json.load(f)
+    ret = []
+    for d in data:
+        text = d["text"]
+        sent_id = d["id"]
+        opinions = []
+        targets = set()
+        for opinion in d["opinions"]:
+            sentiment = opinion["sentiment"]
+            aspect = opinion["aspect"]
+            target_entity = opinion["target_entity"]
+            targets.add(target_entity)
+            opinions.append((target_entity, aspect, sentiment))
+        ret.append((sent_id, text, opinions))
+    return ret
+
+
+def convert_input(data, all_aspects):
+    ret = []
+    for sent_id, text, opinions in data:
+        for target_entity, aspect, sentiment in opinions:
+            if aspect not in all_aspects:
+                continue
+            ret.append((sent_id, text, target_entity, aspect, sentiment))
+        assert "LOCATION1" in text
+        targets = set(["LOCATION1"])
+        if "LOCATION2" in text:
+            targets.add("LOCATION2")
+        for target in targets:
+            aspects = set([a for t, a, _ in opinions if t == target])
+            none_aspects = [a for a in all_aspects if a not in aspects]
+            for aspect in none_aspects:
+                ret.append((sent_id, text, target, aspect, "None"))
+    return ret
+
+
+# wrote the next part myself.
+
+aspect2idx = {
+    "general": 0,
+    "price": 1,
+    "transit-location": 2,
+    "safety": 3,
+    "live": 4,
+    "quiet": 5,
+    "dining": 6,
+    "nightlife": 7,
+    "touristy": 8,
+    "shopping": 9,
+    "green-culture": 10,
+    "multicultural": 11,
+}
+
+
+def all_aspects_logits(data, aspect2idx):
+    aspect_logits = []
+    logit = np.zeros(12)  # taking care of 0 % 12
+    for i, (sent_id, text, target, aspect, sentiment) in enumerate(data, start=1):
+        if i % 12 == 0:
+            aspect_logits.append(logit)
+            logit = np.zeros(12)
+        if sentiment != "None":
+            logit[aspect2idx[aspect]] = 1
+    return aspect_logits
